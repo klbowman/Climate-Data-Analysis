8f807e0431695704bfde8a711a781680dc8be23c
klbowman <klbowman@ucsc.edu>
2021-12-16 10:49:00 -0800

    Flask routes updated

--- app.py
+++ app.py
@@ -75,8 +75,6 @@ def home():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end><br/>"
     )
 
 # Define /api/v1.0/precipitation route
